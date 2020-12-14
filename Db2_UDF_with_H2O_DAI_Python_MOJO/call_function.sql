e9ee32761d84d7985689ebdfa8f7e045295f8dbf
Kelly Schlamb <kschlamb@gmail.com>
2020-12-14 18:08:22 -0500

    Updated samples

--- Db2_UDF_with_H2O_DAI_Python_MOJO/call_function.sql
+++ Db2_UDF_with_H2O_DAI_Python_MOJO/call_function.sql
@@ -21,4 +21,18 @@ VALUES (5.2, 2.0, 1.5, 0.3, IRIS_PM_UDF(5.2, 2.0, 1.5, 0.3));
 VALUES (7.2, 3.0, 5.9, 2.0, IRIS_PM_UDF(7.2, 3.0, 5.9, 2.0));
 VALUES (5.0, 2.8, 1.0, 0.2, IRIS_PM_UDF(5.0, 2.8, 1.0, 0.2));
 
+-- These statements will return the prediction along with the list
+-- of all probabilities (not something you'd practically do, it's
+-- just for example purposes).
+VALUES (4.8, 2.3, 3.0, 1.0, IRIS_PM_PROB_UDF(4.8, 2.3, 3.0, 1.0));
+VALUES (5.2, 2.7, 1.7, 1.0, IRIS_PM_PROB_UDF(5.2, 2.7, 1.7, 1.0));
+VALUES (6.4, 3.0, 5.8, 2.3, IRIS_PM_PROB_UDF(6.4, 3.0, 5.8, 2.3));
+VALUES (5.7, 2.8, 4.2, 1.2, IRIS_PM_PROB_UDF(5.7, 2.8, 4.2, 1.2));
+VALUES (5.1, 2.5, 3.0, 1.2, IRIS_PM_PROB_UDF(5.1, 2.5, 3.0, 1.2));
+VALUES (6.3, 2.8, 5.6, 1.7, IRIS_PM_PROB_UDF(6.3, 2.8, 5.6, 1.7));
+VALUES (7.5, 3.0, 6.6, 2.0, IRIS_PM_PROB_UDF(7.5, 3.0, 6.6, 2.0));
+VALUES (5.2, 2.0, 1.5, 0.3, IRIS_PM_PROB_UDF(5.2, 2.0, 1.5, 0.3));
+VALUES (7.2, 3.0, 5.9, 2.0, IRIS_PM_PROB_UDF(7.2, 3.0, 5.9, 2.0));
+VALUES (5.0, 2.8, 1.0, 0.2, IRIS_PM_PROB_UDF(5.0, 2.8, 1.0, 0.2));
+
 CONNECT RESET;
